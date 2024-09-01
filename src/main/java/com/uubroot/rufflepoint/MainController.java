98a2e72a5f838c9ced47c4ba43ff57ca5edd4164
UuBroot <lolylyt@gmail.com>
2024-09-01 17:06:40 +0200

    renamed files

--- src/main/java/com/uubroot/rufflepoint/MainController.java
+++ src/main/java/com/uubroot/rufflepoint/MainController.java
@@ -3,7 +3,7 @@ package com.uubroot.rufflepoint;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
